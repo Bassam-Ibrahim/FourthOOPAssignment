d7d13cc8d65ef56eebfec0548d247b9a9036367a
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-23 02:13:01 +0300

    DeliveryHelper+DeliveryReport Classes

---
+++ FourthOOPAssignment/DeliveryReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthOOPAssignment
+{
+
+    public class DeliveryReport
+    {
+        public void PrintShipment(ITrackable shipment)
+        {
+            Console.WriteLine(shipment.GetTrackingStatus());
+        }
+
+        public void PrintInsurance(IInsurable shipment)
+        {
+            Console.WriteLine($"{shipment.CalculateInsurance():F2} EGP");
+        }
+    }
+}
