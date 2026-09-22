a9295676ed080afaef9f2d10b3317fb985fc2bfe
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-23 02:06:55 +0300

    Shipment abs class + 3 types Of Delivery Classes

---
+++ FourthOOPAssignment/InternationalShipment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthOOPAssignment
+{
+
+    public class InternationalShipment : Shipment, ITrackable, IInsurable
+    {
+        private string destinationCountry;
+        private decimal customsFee;
+
+        public string DestinationCountry
+        {
+            get { return destinationCountry; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    destinationCountry = value;
+            }
+        }
+
+        public decimal CustomsFee
+        {
+            get { return customsFee; }
+            set
+            {
+                if (value >= 0)
+                    customsFee = value;
+            }
+        }
+
+        public InternationalShipment(string trackingCode, string description,
+                                     decimal weight, decimal deliveryFee,
+                                     DeliveryAddress destination,
+                                     string destinationCountry, decimal customsFee)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            DestinationCountry = destinationCountry;
+            CustomsFee = customsFee;
+        }
+
+        public override decimal EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5) + CustomsFee; }
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("International Shipment");
+            Console.WriteLine($"Tracking Code      : {TrackingCode}");
+            Console.WriteLine($"Destination Country: {DestinationCountry}");
+            Console.WriteLine($"Estimated Cost     : {EstimatedCost} EGP");
+        }
+
+        public string GetTrackingStatus()
+        {
+            return $"Shipment {TrackingCode} has been Delivered.";
+        }
+
+        public decimal CalculateInsurance()
+        {
+            return EstimatedCost * 0.12m;
+        }
+
+        public override void GenerateCustomsReport()
+        {
+            Console.WriteLine($"Customs Report for {DestinationCountry}");
+        }
+    }
+}
---
+++ FourthOOPAssignment/Shipment.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthOOPAssignment
+{
+
+    public abstract class Shipment
+    {
+        private string trackingCode;
+        private string description;
+        private decimal weight;
+        private decimal deliveryFee;
+        private DeliveryAddress destination;
+
+         public string TrackingCode
+        {
+            get { return trackingCode; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    trackingCode = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    description = value;
+            }
+        }
+
+        public decimal Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0)
+                    weight = value;
+            }
+        }
+
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+            private set
+            {
+                if (value > 0)
+                    deliveryFee = value;
+            }
+        }
+
+        public DeliveryAddress Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        public abstract decimal EstimatedCost { get; }
+        public abstract void PrintShipment();
+
+        protected Shipment(string trackingCode, string description,
+                           decimal weight, decimal deliveryFee,
+                           DeliveryAddress destination)
+        {
+            TrackingCode = trackingCode;
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+
+
+        public void UpdateDeliveryFee(decimal newFee)
+        {
+            if (newFee > 0)
+                DeliveryFee = newFee;
+        }
+
+        public void UpdateWeight(decimal newWeight)
+        {
+            Weight = newWeight;
+        }
+
+        public void UpdateWeight(decimal newWeight, decimal extraPackingWeight)
+        {
+            Weight = newWeight + extraPackingWeight;
+        }
+
+        public virtual void GenerateCustomsReport()
+        {
+            Console.WriteLine("Base customs report.");
+        }
+    }
+}
