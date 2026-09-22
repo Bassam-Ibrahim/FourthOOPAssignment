cdd1f9ff87c8b5a65ad2ecb4bf0d2de2ee2d942d
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-23 02:00:51 +0300

    DeliveryAddress Struct + ITrackable Interface

---
+++ FourthOOPAssignment/ITrackble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthOOPAssignment
+{
+    public interface ITrackable
+    {
+        string GetTrackingStatus();
+    }
+
+    public interface IInsurable
+    {
+        decimal CalculateInsurance();
+    }
+}
--- FourthOOPAssignment/Program.cs
+++ FourthOOPAssignment/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Theo Questions
+        
+        #endregion
+    
+        }
+}
