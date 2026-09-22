3c44b27fe6a093eade99ac30655a8ac48267333a
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-09-23 02:11:20 +0300

    DeliveryCenter,Driver,Completed,PriorityInternalional Classes

---
+++ FourthOOPAssignment/CompletedShipment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthOOPAssignment
+{
+
+    public sealed class CompletedShipment : Shipment
+    {
+        public CompletedShipment(string trackingCode, string description,
+                                 decimal weight, decimal deliveryFee,
+                                 DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+
+        public override decimal EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5); }
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("Completed Shipment");
+            Console.WriteLine($"Tracking Code : {TrackingCode}");
+            Console.WriteLine($"Description   : {Description}");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+    }
+}
---
+++ FourthOOPAssignment/DeliveryCenter.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourthOOPAssignment
+{
+    using System;
+
+    public class DeliveryCenter
+    {
+        private Shipment[] shipments;
+        private int count;
+
+        public string CenterName { get; set; }
+        public Driver Driver { get; set; }   
+
+        public DeliveryCenter(string centerName)
+        {
+            CenterName = centerName;
+            shipments = new Shipment[20];
+            count = 0;
+        }
+
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < count)
+                    return shipments[index];
+                return null;
+            }
+            set
+            {
+                if (index >= 0 && index < count)
+                    shipments[index] = value;
+            }
+        }
+
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (shipments[i].TrackingCode == trackingCode)
+                        return shipments[i];
+                }
+                return null;
+            }
+        }
+
+        public bool AddShipment(Shipment shipment)
+        {
+            if (count < shipments.Length)
+            {
+                shipments[count] = shipment;
+                count++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveShipment(string trackingCode)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (shipments[i].TrackingCode == trackingCode)
+                {
+                    for (int j = i; j < count - 1; j++)
+                        shipments[j] = shipments[j + 1];
+
+                    shipments[count - 1] = null;
+                    count--;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void PrintAllShipments()
+        {
+            Console.WriteLine($"Delivery Center : {CenterName}");
+            Console.WriteLine();
+
+            for (int i = 0; i < count; i++)
+            {
+                shipments[i].PrintShipment();
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintTrackingStatuses()
+        {
+            Console.WriteLine("Tracking Status");
+
+            for (int i = 0; i < count; i++)
+            {
+                if (shipments[i] is ITrackable t)
+                    Console.WriteLine(t.GetTrackingStatus());
+            }
+        }
+    }
+}
